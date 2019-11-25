8903b387ee5c7f7ff963790a1bc69bb8ecfa2712
Nick <nboober@terpmail.umd.edu>
2019-11-25 12:27:10 -0500

    Done.

--- sushi-saga-client/src/components/AddFunds.js
+++ sushi-saga-client/src/components/AddFunds.js
@@ -5,7 +5,7 @@ const AddFunds = (props) => {
         <div className="addFunds">
           <b>Add Funds: </b>
           <br/>
-          <input type="number" placeholder="0" min="1" className="addFunds"/>
+          <input type="number" placeholder="0" min="0" className="addFunds"/>
           <button onClick={(event) => props.addFunds(event)} className="addFunds" >Add Funds</button>
         </div>
     )
